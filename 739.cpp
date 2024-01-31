502b0f1c37965b158a19e958a4b8fa89c4400db9
Riddhi Gupta <ridhie143@gmail.com>
2024-01-31 20:02:41 +0530

    Create 739.cpp

---
+++ 739.cpp
@@ -0,0 +1,28 @@
+#pragma GCC optimize("O3", "unroll-loops")
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        int n=temperatures.size();
+        vector<int> ans(n, 0);
+        vector<array<int,2>> st;
+        st.push_back({temperatures[n-1], n-1});
+        for(int i=n-2; i>=0; i--){
+            while(!st.empty() && temperatures[i] >= st.back()[0])
+                st.pop_back();
+
+            if (!st.empty())
+                ans[i] = st.back()[1] - i;
+
+            st.push_back({temperatures[i], i});
+        }
+        return ans;
+    }
+};
+
+auto init = []()
+{ 
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
