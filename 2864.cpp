e8ec197bd427e499ebfad2eda5a37a7482812b50
Riddhi Gupta <ridhie143@gmail.com>
2024-03-01 11:05:57 +0530

    Update 2864.cpp

--- 2864.cpp
+++ 2864.cpp
@@ -1,12 +1,18 @@
 #pragma GCC optimize("O3", "unroll-loops")
-char* maximumOddBinaryNumber(char* s){
-    int n, n1=0, n0;
-    for(n=0; s[n]!='\0'; n++){
-        n1+=(s[n]=='1');
+class Solution {
+public:
+    string maximumOddBinaryNumber(string& s) {
+        int n=s.size(), r=n-1, l=0;
+        if (s[r]!='1') {// last bit must be 1
+            while(s[l]!='1') l++;
+            swap(s[l], s[r]);
+        }
+        l=0, r=n-2;//Rewind l, r
+        while(l<r){
+            while(l<n && s[l]=='1') l++;
+            while(r>=0 && s[r]=='0') r--;
+            if (l<r) swap(s[l], s[r]);
+        }
+        return s;
     }
-    n0=n-n1;
-    s[n-1]='1';
-    memset(s, '1', n1-1 );
-    memset(s+n1-1, '0', n0);
-    return s;
-}
+};
