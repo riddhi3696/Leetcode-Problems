644cc4b6e34a64a5ef62c26e916b5d0fc8b66c8d
Riddhi Gupta <ridhie143@gmail.com>
2024-03-09 13:01:36 +0530

    Create 2540.cpp

---
+++ 2540.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int getCommon(vector<int>& nums1, vector<int>& nums2) {
+        
+        int i=0;
+        int j=0;
+
+        while(i<nums1.size() && j<nums2.size()){
+            if(nums1[i]==nums2[j]){
+                return nums1[i];
+            }
+            else if(i<nums1.size() && j<nums2.size() && nums1[i]<nums2[j]){
+                i++;
+            }
+            else if(i<nums1.size() && j<nums2.size() && nums1[i]>nums2[j]){
+                j++;
+            }
+        }
+        return -1;
+    }
+};
